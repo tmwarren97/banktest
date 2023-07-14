0f51d8a954b7b96d6892ac74d58ee6aa530e8ff9
rsivaranmaiah <rsivaranmaiah@yahoo.com>
2023-07-13 23:05:09 -0400

    Ravi All three tests passed - again

--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -40,7 +40,7 @@ public class CustomerTest {
     public void testOneAccount(){
         Customer oscar = new Customer("Oscar").openAccount(new SavingsAccount());
         assertEquals(1, oscar.getNumberOfAccounts());
-        assertEquals(1, oscar.getNumberOfAccounts());
+        assertEquals(1, oscar.getNumberOfAccounts());  // done
     }
 
     @Test
