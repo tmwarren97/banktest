bdd7b25fa09e9809402e8abc65c16923d5b61aaa
Steven <62080300+fcheny2k@users.noreply.github.com>
2023-10-20 09:59:14 -0400

    Update Account.java

--- src/main/java/com/abc/account/Account.java
+++ src/main/java/com/abc/account/Account.java
@@ -1,5 +1,6 @@
 package com.abc.account;
 
+import com.abc.AccountType;
 import com.abc.Transaction;
 import com.abc.calculator.InterestCalculator;
 
@@ -8,28 +9,41 @@ import java.util.List;
 
 public class Account {
 
-    public List<Transaction> transactions;
+    private static final String ACCOUNT_TYPE_FORMAT  = "%s Accounts\n";
+    protected List<Transaction> transactions = new ArrayList<>();
     private final InterestCalculator interestCalculator;
-
+    private double accountBalance;
+    
     public Account(InterestCalculator interestCalculator) {
         this.interestCalculator = interestCalculator;
-       this.transactions = new ArrayList<Transaction>();
     }
 
     public void deposit(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
-        } else {
-            transactions.add(new Transaction(amount));
         }
+        accountBalance += amount;
+        transactions.add(new Transaction(amount, TransactionType.DEPOSIT.name()));
+        
     }
 
+    public List<Transaction> getTransactions()
+    {
+        return transactions;
+    }
+    
     public void withdraw(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
-        } else {
-            transactions.add(new Transaction(-amount));
         }
+
+        if (amount > accountBalance)
+        {
+            throw new IllegalArgumentException("Insufficient fund");
+        }
+        accountBalance -= amount;
+        transactions.add(new Transaction(-amount, TransactionType.WITHDRAWAL.name()));
+        
     }
 
     public double interestEarned() {
@@ -38,24 +52,32 @@ public class Account {
     }
 
     public double sumTransactions() {
-        return checkIfTransactionsExist(true);
+        return calculateTotalTransactionsAmount();
     }
 
-    private double checkIfTransactionsExist(boolean checkAll) {
-        double amount = 0.0;
-        for (Transaction t : transactions)
-            amount += t.amount;
-        return amount;
+    public boolean checkIfTransactionExists()
+    {
+         return !transactions.isEmpty();   
     }
-
+    
     public String getAccountName() {
-        return "Default";
+        return AccountType.DEFAULT.name();
     }
 
-    private static final String ACCOUNT_TYPE_FORMAT  = "%s Accounts\n";
-
+    public double getAccountBalance()
+    {
+        return accountBalance;
+    }
+    
     public String printAccountType() {
         return String.format(ACCOUNT_TYPE_FORMAT, getAccountName());
+    }
+    
+    private double calculateTotalTransactionsAmount() {
+        double amount = 0.0;
+        for (Transaction transaction : transactions)
+            amount += transaction.amount;
+        return amount;
     }
 
 }
