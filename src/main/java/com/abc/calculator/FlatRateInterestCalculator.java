4e5757ef416e492432229ff671e30bb960e9343c
Steven <62080300+fcheny2k@users.noreply.github.com>
2023-10-20 10:10:37 -0400

    Update FlatRateInterestCalculator.java

--- src/main/java/com/abc/calculator/FlatRateInterestCalculator.java
+++ src/main/java/com/abc/calculator/FlatRateInterestCalculator.java
@@ -2,16 +2,13 @@ package com.abc.calculator;
 
 public class FlatRateInterestCalculator implements InterestCalculator {
 
-    public static final double ONE_PERCENT = 0.05;
+    private static final double ONE_PERCENT = 0.05;
 
-    private final double flatRate;
-
-    public FlatRateInterestCalculator(double flatRate) {
-        this.flatRate = flatRate;
+    public FlatRateInterestCalculator() {
     }
 
     @Override
     public double calculateInterest(double amount) {
-        return amount * flatRate;
+        return amount * ONE_PERCENT;
     }
 }
